86f2d098cb486422b9158f779897dd1e46555680
FranckErnewein <franck.ernewein@gmail.com>
2024-11-05 08:56:04 +0100

    remove haskell

--- game/app/Main.hs
+++
@@ -1,4 +0,0 @@
-import Vector
-
-main :: IO ()
-main = print (add (1, 1) (3, 5))
--- src/load-haskell-wasm.ts
+++
@@ -1,9 +0,0 @@
-import wasm from "./vector.wasm?init";
-
-import { WASI } from "@bjorn3/browser_wasi_shim";
-
-const wasi = new WASI([], [], []);
-
-wasm({ wasi_snapshot_preview1: wasi.wasiImport }).then((instance) => {
-  console.log(instance);
-});
